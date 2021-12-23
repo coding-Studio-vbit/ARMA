70067fa027bd4667e9f480c490f4d57456c1c6c0
Sai Kiran Boddu <bsaikiran618@gmail.com>
2021-12-23 11:33:21 +0530

    First commit :)

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Welcome to A.R.M.A :)
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
