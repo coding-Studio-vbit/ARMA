c506bba3ef127eaa419e74a2c5fd90f41e94161a
Sai Kiran Boddu <bsaikiran618@gmail.com>
2021-12-29 19:25:46 +0530

    added event model

--- server/src/models/event.js
+++ server/src/models/event.js
@@ -1,10 +1,69 @@
 const mongoose = require("mongoose");
 
-const role = new mongoose.Schema({
-	roleName: {type: String, required: true},
-	//arbitrary list of strings that make sense only to the server code
-	permissions:[String]
+const event = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: [
+      process.env.MIN_NAME_LENGTH,
+      `Name must be minimum ${process.env.MIN_NAME_LENGTH} characters`,
+    ],
+    maxLength: [
+      process.env.MAX_NAME_LENGTH,
+      `Name must be max ${process.env.MAX_NAME_LENGTH} characters`,
+    ],
+  },
+  description:{
+	  type:String,
+	  minLength:[process.env.MIN_EVENT_DESCRIPTION_LENGTH, `description has to consist of minimum ${process.env.MIN_EVENT_DESCRIPTION_LENGTH} characters.`],
+	  maxLength:[process.env.MAX_EVENT_DESCRIPTION_LENGTH, `description has to consist of maximum ${process.env.MAX_EVENT_DESCRIPTION_LENGTH} characters.`]
+  },
+  forumID: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "forums",
+  },
+  attendanceDocID: {
+    required: true,
+    type: mongoose.Schema.Typses.ObjectId,
+    ref: "attendance",
+  },
+  eventStatus: {
+    type: String,
+    default: "APPROVAL PENDING",
+    enum: [
+      "APPROVAL PENDING",
+      "REQUESTED CHANGES",
+      "APPROVED",
+      "REJECTED",
+      "COMPLETE",
+    ],
+  },
+  hasBudget: {
+    type: Boolean,
+    required: true,
+  },
+  budgetStatus: {
+    type: String,
+    default: "APPROVAL PENDING",
+    enum: ["APPROVAL PENDING", "REQUESTED CHANGES", "APPROVED", "REJECTED"],
+  },
+  eventDates: [Date],
+  eventProposalDocPath: {
+    type: String,
+    required: true,
+  },
+  budgetDocPath: {
+    type: String,
+    required: this.hasBudget,
+  },
+  reportDocPath: {
+    type: String,
+  },
+  mediaFilePaths:[{type:String}],
+  FOComments:String,
+  SACComments:String,
 });
 
-const roles = mongoose.model("roles", role)
-mongoose.exports = roles
+const events = mongoose.model("events", evenst);
+mongoose.exports = events;
--- server/src/models/role.js
+++ server/src/models/role.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const role = new mongoose.Schema({
-	roleName: {type: String, required: true},
+	name: {type: String, required: true},
 	//arbitrary list of strings that make sense only to the server code
 	permissions:[String]
 });
