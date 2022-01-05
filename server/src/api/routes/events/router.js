a2d62bb7c3dd978dc249b77d88d30dae5628b825
Sai Kiran Boddu <bsaikiran618@gmail.com>
2022-01-05 12:11:21 +0530

    cleansed routing files

--- server/src/api/routes/events/router.js
+++ server/src/api/routes/events/router.js
@@ -1,42 +1,7 @@
 const router = require("express").Router();
-const events = require("../../../models/event");
 const checkRolePermissions = require("../../../services/util/checkRole");
+const controller = require("../../../services/events/controller");
 
-router.get("/", checkRole(req, res, next, 'GET_EVENTS') ,(req, res) => {
-  //For pagination
-  let page = req.query.page ? Number(req.query.page) : 1;
-  let limit = req.query.limit ? Number(req.query.limit) : 1000000;
-
-  //For filters
-  let where = {};
-  if (req.query.forumID) where.forumID = req.query.forumID;
-  if (req.query.eventStatus) where.eventStatus = req.query.eventStatus;
-
-  //For sorting
-  let sort = {};
-  if (req.query.orderBy && req.query.order)
-    sort[req.query.orderBy] = req.query.order;
-  else sort = { name: "asc" };
-
-  try {
-    result = await events
-      .find(where)
-      .skip((page - 1) * limit)
-      .limit(limit)
-      .sort(sort);
-    const total = await events.count(where);
-    res.json(
-      response({ data: result, total: total }, process.env.SUCCESS_CODE)
-    );
-  } catch (error) {
-    console.log(error);
-    res.json(
-      response(
-        { message: "event data fetch error" },
-        process.env.FAILURE_CODE
-      )
-    );
-  }
-});
+router.get("/", checkRolePermissions(req, res, next, "GET_EVENTS"), controller.getEvents);
 
 module.exports = router;
--- server/src/api/routes/forum/router.js
+++ server/src/api/routes/forum/router.js
@@ -1,34 +1,6 @@
 const router = require("express").Router();
-const events = require("../../../models/event");
-const response = require("../../../services/util/response");
-const {welcomeTemplate} = require("../../../email_templates/templates");
-const mailer = require("../../../services/util/mailer");
+const controller = require("../../../services/forum/controller");
 
-router.get("/dashboard", async (req, res) => {
-  try {
-    let myEvents = await events.find({ forumID: req.user._id });
-    let statistics = { engagement: 4, total: myEvents.length };
-    console.log(myEvents);
-    res.json(
-      response(
-        { events: myEvents, statistics: statistics },
-        process.env.SUCCESS_CODE
-      )
-    );
-  } catch (err) {
-    console.log(err);
-    res.json(response(error, process.env.FAILURE_CODE));
-  }
-});
-
-// router.get('/test', async (req, res)=>{
-//   mailer.sendMail("bsaikiran618@gmail.com", welcomeTemplate, {userName: "Sai Kiran"})
-//   .then(response=>{
-//     res.json("Kewl kewl sent");
-//   })
-//   .catch(error=>{
-//     res.json("boo boo");
-//   })
-// })
+router.get("/dashboard", controller.dashboard);
 
 module.exports = router;
