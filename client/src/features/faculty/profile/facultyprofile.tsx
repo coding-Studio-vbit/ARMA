362220a9ba8c1195adef6504a3e9fde17a80a937
SidMalladi <siddumalladi1234@gmail.com>
2022-01-06 13:18:30 +0530

    update FacultyProfile

--- client/src/features/faculty/profile/facultyprofile.tsx
+++ client/src/features/faculty/profile/facultyprofile.tsx
@@ -1,35 +1,47 @@
-import React from 'react';
-
+import React from "react";
+import { InputField } from "../../../components/InputField/InputField";
 
 function FacultyProfile() {
-    return (
-        <body className='min-h-screen  bg-[#E5E5EA]'>
-        <div>
-            <div className='flex justify-end mt-14 pr-10 mb-20'>
-            <button className='outlineBtn'>LOGOUT</button>
-            </div>
-            <div className='flex flex-col w-[542px] items-center m-auto'>
-            <p className='text-center item-center text-3xl text-arma-blue'>Siddharth Malladi</p>
-            
-            <p className = 'text-black mt-4 mb-10 text-lg'>Faculty</p>
-            <div className='flex gap-x-11 mb-10'>
-            <input className='shadow appearance-none border rounded-[24px] py-2 px-9 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type="text" placeholder='Rollnumber' />
-            <input className='shadow appearance-none border rounded-[24px] py-2 px-9 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type="text" placeholder='Email' />
-            </div>
-            <div className='flex gap-x-11 mb-10'>
-            <input className='shadow appearance-none border rounded-[24px] py-2 px-9 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type="text" placeholder='Designation' />
-            <input className='shadow appearance-none border rounded-[24px] py-2 px-9 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' type="text" placeholder='Department' />
-            </div>
-            <input className='shadow appearance-none border rounded-[24px] py-2 px-9 text-gray-700 leading-tight focus:outline-none focus:shadow-outline self-start' type="text" placeholder='Phone Number' />
-
-            
-            </div>
+  return (
+    <body className="min-h-screen">
+      <div>
+        <div className="flex justify-end mt-14 pr-10 mb-20">
+          <button className="outlineBtn">LOGOUT</button>
         </div>
-        </body>
-    );
+        <div className="flex flex-col w-[542px] items-center m-auto">
+          <p className="text-center item-center text-3xl text-arma-blue">
+            Siddharth Malladi
+
+          </p>
+
+          <p className="text-black mt-4 mb-10 text-lg">Faculty</p>
+          <div className="flex gap-x-10 mb-10 w-full">
+            <InputField className="mb-5" name="Email" onChange={(e) => {}} />
+            <InputField className="mb-5" name="Name" onChange={(e) => {}} />
+          </div>
+          <div className="flex gap-x-10 mb-10 w-full">
+            <InputField
+              className="mb-5"
+              name="Designation"
+              onChange={(e) => {}}
+            />
+            <InputField
+              className="mb-5"
+              name="Department"
+              onChange={(e) => {}}
+            />
+          </div>
+          <div className="w-full	">
+            <InputField
+              className="mb-5"
+              name="Phone Number"
+              onChange={(e) => {}}
+            />
+          </div>
+        </div>
+      </div>
+    </body>
+  );
 }
 
-export {FacultyProfile}
-
-
-
+export { FacultyProfile };
