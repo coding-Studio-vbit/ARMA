bb14a1e6283d8f5f0d7deaa3e8d79ff27774a8ff
RevantArk <revantark@gmail.com>
2022-01-12 12:53:32 +0530

    Report and media Done

--- client/src/routes/ForumRoutes.tsx
+++ client/src/routes/ForumRoutes.tsx
@@ -3,6 +3,8 @@ import Footer from "../components/CustomFooter";
 import Navbar from "../components/CustomNavbar";
 import Dashboard from "../features/forum/main_dashboard/Dashboard";
 import { EventVenue } from "../features/forum/event_venue";
+import { ReportAndMedia } from "../features/forum/event_reports_and_media/reportsAndMedia";
+import ForumEventDashboard from "../features/forum/event_dashboard/forumEventDashboard";
 
 const ForumRoutes = () => {
   return (
@@ -10,7 +12,11 @@ const ForumRoutes = () => {
       <Navbar navItems={[]}/>
         <Routes>
           <Route path="/" element={<Dashboard/>} />
+          <Route path="/eventDashboard" element={<ForumEventDashboard/>} />
+
           <Route path="/event-venue" element={<EventVenue/>} />
+          <Route path="/reportAndMedia" element={<ReportAndMedia/>} />
+          
         </Routes>
       <Footer />
     </div>
