0e6db803ae6b815379ac0f6cab567d5e9fc83ddb
Prashanith <prashanith007@gmail.com>
2022-01-14 16:18:28 +0530

    Faculty Dashboard Calendar Update

--- client/src/features/faculty/dashboard/facultyDashBoard.tsx
+++ client/src/features/faculty/dashboard/facultyDashBoard.tsx
@@ -8,6 +8,7 @@ import { Calendar,DayValue,Day } from "react-modern-calendar-datepicker";
 interface EventInfo {
     forum:string;
     event:string;
+    date:Day
 }
 
 function FacultyDashBoard() {
@@ -19,38 +20,93 @@ function FacultyDashBoard() {
     const [todaysEvents, setTodaysEvents] = useState<EventInfo[]>();
     const defaultValue= [
         {
-            year: 2022,
-            month: 1,
             day: 5,
+            month: 1,
+            year: 2022,
         },
         {
-            year: 2022,
-            month: 1,
             day: 6,
+            month: 1,
+            year: 2022,
         },
         {
-            year: 2022,
-            month: 1,
             day: 7,
+            month: 1,
+            year: 2022,
         },
         {
-            year: 2022,
-            month: 1,
             day: 8,
-        }
+            month: 1,
+            year: 2022,
+        },        
     ];
-    
-    const [selectedDay, setSelectedDay] = useState(defaultValue);
+
+    const [eventDays, setEventDays] = useState(defaultValue);
+    const [selectedDate, setSelectedDate] = useState<Day>({
+        day:new Date().getDate(),
+        month:new Date().getMonth()+1,
+        year: new Date().getFullYear(),
+    },);
+
+    function handleSelectedDate(days:Day[]){
+        let difference:Day[] = eventDays
+                 .filter(x => !days.includes(x))
+                 .concat(days.filter(x => !eventDays.includes(x)));  
+        console.log(difference[0]);
+        setSelectedDate(difference[0]);
+    }
+
+    function filterWithDates():EventInfo[]{
+        let x= todaysEvents?.filter((element)=>
+            element.date.day== selectedDate?.day && 
+            element.date.month== selectedDate?.month &&
+            element.date.year== selectedDate?.year  
+        )
+        if(x!=undefined){
+            return x;
+        }else{
+            return [];
+        }
+    }
 
     async function fetchData(){
         setTimeout(() => {
             setLoading(false);
             setUsername("Prashanith");
             let li=[
-                {forum:"codeStream",event:"codeStream_1"},
-                {forum:"IEEE",event:"IEEE Xtreme"},
-                {forum:"codingStudio();",event:"CodeCraft"},
-                {forum:"Stumagz",event:"Talks"},     
+                {
+                    forum:"codeStream",
+                    event:"codeStream_1",
+                    date:{
+                        day: 8,
+                        month: 1,
+                        year: 2022,
+                    }
+                },
+                {
+                    forum:"IEEE",
+                    event:"IEEE Xtreme",
+                    date:{
+                        day: 7,
+                        month: 1,
+                        year: 2022,
+                    },
+                },
+                {
+                    forum:"codingStudio();",event:"CodeCraft",date:{
+                        day: 7,
+                        month: 1,
+                        year: 2022,
+                    },
+                },
+                {
+                    forum:"Stumagz",event:"Talks",
+                    date:{
+                        day: 7,
+                        month: 1,
+                        year: 2022,
+                    },
+                },     
             ];
             setPendingRequests(7);
             setNewRequests(4);
@@ -77,7 +133,7 @@ function FacultyDashBoard() {
                     {/* Current Requests */}
                     <div className='w-full sm:w-2/3 lg:w-1/2 mx-4 px-10 py-12
                     arma-card-gradient text-white text-2xl
-                    shadow-2xl rounded-2xl h-72 overflow-y-scroll currentRequest' >
+                    shadow-2xl rounded-2xl h-72 overflow-y-scroll currentRequest cursor-pointer' >
                         <p>Current Requests</p>
                         <ul className='list-disc list-inside text-xl '>
                             {
@@ -90,13 +146,13 @@ function FacultyDashBoard() {
                     <div className='w-full sm:w-2/3 lg:w-1/2 mx-4 flex justify-center gap-4 lg:gap6 xl:gap-10 items-center h-72'>
                         {/* New Requests */}
                         <div className='w-1/2 h-full shadow-2xl rounded-2xl text-center 
-                            flex flex-col justify-center'>
+                            flex flex-col justify-center  cursor-pointer'>
                             <p className='text-lg'>New Requests</p>
                             <div className="pr-3 arma-text-gradient text-transparent bg-clip-text text-9xl  md:text-7xl lg:text-8xl">{newRequests}</div>
                         </div>
                         {/* Pending */}
                         <div className='w-1/2 h-full shadow-2xl rounded-2xl text-center 
-                            flex flex-col justify-center'>
+                            flex flex-col justify-center  cursor-pointer'>
                             <p className='text-xl'>Pending</p>
                             <div className="pr-3 arma-text-gradient text-transparent bg-clip-text text-9xl md:text-7xl lg:text-8xl">{pendingRequests}</div>
                         </div>
@@ -104,26 +160,33 @@ function FacultyDashBoard() {
                 </div>
 
                 <div className='flex flex-row gap-y-10 flex-wrap  lg:flex-nowrap  justify-center lg:justify-start w-full md:w-5/6 lg:w-11/12  xl:w-9/12 gap-4 xl:gap-6'>
-                    <div className='w-full sm:w-2/3 lg:w-1/2 mx-4 px-10 py-12
-                    text-2xl shadow-2xl rounded-2xl h-72 overflow-y-scroll currentRequest white'>
-                        <p className='arma-text-gradient text-transparent bg-clip-text'>Today's Events</p>
-                        <ul className='list-disc list-inside text-xl'>
-                            {
-                                todaysEvents?.map((element)=>{
-                                    return <li>{element.event}</li> 
-                                })
-                            }
-                        </ul>                          
-                    </div>
-                    <div className='w-full sm:w-2/3 lg:w-1/2 mx-4 text-2xl border-2 border-grey-600 shadow-2xl rounded-2xl overflow-y-scroll currentRequest white'>
+                    <div className='w-full sm:w-2/3 lg:w-1/2 xl:w-5/12 mx-4 text-2xl border-2 border-grey-600 shadow-2xl rounded-2xl overflow-y-scroll currentRequest white'>
                         <Calendar
-                            value={selectedDay}
-                            // onChange={setSelectedDay}
+                            value={eventDays}
+                            onChange={(e)=>handleSelectedDate(e)}
                             colorPrimary="#0B5B8A"
                             calendarClassName="responsiveCalendar"
                             shouldHighlightWeekends
                         />
                     </div>
+                    
+                    <div className='w-full sm:w-2/3 lg:w-1/2 mx-4 px-10 py-12
+                        text-2xl shadow-2xl rounded-2xl h-72 overflow-y-scroll currentRequest white'>
+                        <p className='arma-text-gradient text-transparent bg-clip-text'>
+                        Today's Events - {selectedDate.day+"/"+selectedDate.month+"/"+selectedDate.year}</p>
+                        {
+                            selectedDate!=null &&
+                            filterWithDates().length>0?
+                            <ul className='mt-3 list-disc list-inside text-xl'>
+                            {
+                                filterWithDates().map((e)=>{
+                                    return <li>{e.event}</li>
+                                })
+                            }
+                            </ul>:
+                            <p className='text-xl text-teal-600 mt-4  my-auto h-full'>No Events Today</p>
+                        }
+                    </div>                    
 
                 </div>
             </div>
