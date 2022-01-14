a7edf105b631364dffc0b2297291f18638b30db6
Prashanith <prashanith007@gmail.com>
2022-01-14 11:06:51 +0530

    Toggle Animations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,26 @@
 import './App.css';
+import ToggleSwitch from './components/CustomToggleSwitch';
 // import FacultyDashBoard from './features/faculty/dashboard/facultyDashBoard';
 // import ForumEventDashboard from './features/forum/event_dashboard/forumEventDashboard';
 import { UserProvider } from './providers/user/UserProvider';
 import AllRoutes from './routes/routes';
+import {useState} from 'react'
 
 function App() {
+
+  const [toggle, setToggle] = useState<boolean>(false)
+
   return (
     <div className="App font-inter">
-      <UserProvider> 
+      {/* <UserProvider> 
        <AllRoutes />
-      </UserProvider>
+      </UserProvider> */}
+
+      {/* <div className='p-20'>
+      <ToggleSwitch isEnabled={toggle} toggleSwitch={setToggle} />
+
+
+      </div> */}
     </div>
   );
 }
--- client/src/components/CustomToggleSwitch.tsx
+++ client/src/components/CustomToggleSwitch.tsx
@@ -9,11 +9,16 @@ function ToggleSwitch({isEnabled,toggleSwitch}:Toggle) {
     return (
         <div onClick={()=>toggleSwitch(!isEnabled)}
             className={
-                !isEnabled?
-                "flex justify-start items-center bg-arma-toggle h-6 w-16 p-0 border-2 border-transparent rounded-full cursor-pointer":
-                "flex justify-end items-center bg-arma-toggle h-6 w-16 p-0 border-2 border-transparent rounded-full cursor-pointer"
+                "flex  items-center bg-arma-toggle h-5 w-14 p-0 border-2 border-transparent rounded-full cursor-pointer"
             }>
-            <span className="rounded-full shadow h-7 w-7 bg-arma-dark-blue transform ring-0  transition ease-in-out duration-200">
+            <span 
+
+            className={
+                isEnabled?
+                "rounded-full translate-x-8 shadow h-6 w-6 bg-arma-dark-blue transform ring-0  transition ease-in-out duration-200":
+                "rounded-full -translate-x-1 shadow h-6 w-6 bg-arma-dark-blue transform ring-0  transition ease-in-out duration-200"
+
+            }>
             </span>
         </div>
     )
