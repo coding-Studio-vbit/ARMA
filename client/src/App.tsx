c6050d59d2962a3e5f28887e15af5bd6cd4fde9a
balgurikalyan <balgurikalyan711@gmail.com>
2022-01-14 17:35:51 +0530

    update event UI fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css';
+import CreateEvent from './features/forum/create_event/createEvent';
+import UpdateEventDetails from './features/forum/event_details/updateEventDetails';
 import { UserProvider } from './providers/user/UserProvider';
 import AllRoutes from './routes/routes';
 
@@ -7,9 +9,10 @@ function App() {
 
   return (
     <div className="App font-inter">
-      <UserProvider> 
+      {/* <UserProvider> 
        <AllRoutes />
-      </UserProvider>
+      </UserProvider> */}
+      <UpdateEventDetails />
     </div>
   );
 }
--- client/src/features/forum/event_details/updateEventDetails.tsx
+++ client/src/features/forum/event_details/updateEventDetails.tsx
@@ -5,26 +5,28 @@ import {
   } from "@material-ui/icons";
 const UpdateEventDetails=()=>{
     const [pdf1, setPdf1] = useState<File>();
+    const [desc,setDesc]=useState("codeCraft is the flagship event conducted by coding.Studio( ); every year that aims to rmote competitive programming culture in our college. ")
     return(
-        <div className="bg-gray-100 h-full mb-10">
+        <div className="bg-gray-400 h-full mb-10">
             <Navbar navItems={[]}  />
-            <h1 className="font-sans text-arma-dark-blue font-semibold text-xl md:text-4xl inline-block ml-4 md:ml-28 mt-2">c.S(); SoC - Event Details</h1>    
-            <div className="mx-4 md:mx-28 mt-12 md:mt-20">
+            <h1 className="mt-28 text-arma-dark-blue font-semibold text-xl md:text-4xl inline-block ml-4 md:ml-28 ">c.S(); SoC - Event Details</h1>    
+            <div className="mx-4 md:mx-28 mt-4 md:mt-8">
                 <div>
                     <div className="flex">
                         <h1 className="text-gray-500 text-md md:text-xl">Event Description</h1>
                         <span className="material-icons text-arma-blue ml-3">help</span>
                     </div>
-                    <div className="bg-white border border-solid rounded-2xl border-white mt-3 p-5 h-fit text-xs md:text-sm">
-                        <p className="text-justify">
+                    <div className="bg-white border border-solid rounded-2xl border-white mt-3 p-5 w-5/6 text-xs md:text-sm">
+                        {/* <p className="text-justify">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos doloribus corrupti quod nostrum eius distinctio consequuntur, et inventore tenetur cum ipsum accusantium sapiente dolor magni voluptates excepturi non impedit nihil.
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptates at perspiciatis aperiam alias consectetur ex? Eos culpa rem ipsa ducimus accusamus mollitia laudantium. Quibusdam corrupti ullam omnis cupiditate maiores.
-                        </p>
+                        </p> */}
+                        <textarea value={desc} onChange={(e)=>{setDesc(e.target.value)}} className="" />
                     </div>
                 </div>
                 <div className="md:flex md:flex-col-2 justify-evenly ">
                     <div className="md:w-1/2 md:mr-2  mt-6 md:mt-12">
-                        <div className="flex justify-center">
+                        <div className="flex md:justify-center">
                             <h1 className="text-gray-500 text-md md:text-xl">SAC Comments</h1>
                             <span className="material-icons ml-3 text-arma-blue">feedback</span>
                         </div>
@@ -35,7 +37,7 @@ const UpdateEventDetails=()=>{
                         </div>
                     </div>
                     <div className="md:w-1/2 md:ml-2  mt-6 md:mt-12">
-                        <div className="flex justify-center">
+                        <div className="flex md:justify-center">
                             <h1 className="text-gray-500 text-md md:text-xl ">Event Proposal Document</h1>
                             <span className="material-icons text-arma-blue ml-3">library_books</span>
                         </div>
@@ -61,7 +63,7 @@ const UpdateEventDetails=()=>{
                         </div>                        
                     </div>
                 </div>
-                <div className=" flex space-x-2 justify-center mt-10 text-sm md:text-md "><button className="btn">Update</button></div>
+                <div className=" flex space-x-2 justify-center mt-10 text-sm md:text-md "><button className="btn" onClick={()=>{console.log(desc)}}>Update</button></div>
             </div>
         </div>
     )
