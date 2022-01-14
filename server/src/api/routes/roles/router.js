0be09d0ad63cb00f96f4a782e79425341f66002c
shivanishivalenka <shivalenkashivani@gmail.com>
2022-01-15 00:32:26 +0530

    add Roles router

---
+++ server/src/api/routes/roles/router.js
@@ -0,0 +1,13 @@
+const router = require("express").Router()
+const checkRolePermissions = require("../../../services/util/checkRole")
+const controller = require("../../../services/roles/controller")
+
+// ADD ROLES
+
+router.post("/addRole",(req,res,next)=>{
+    return checkRolePermissions(req, res, next, "ADD_ROLE")
+    },
+    controller.addRoles
+)
+
+module.exports = router;
