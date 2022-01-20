ba15b8b285796c1c9a80d9b9a7d3facb6d6c4bef
Prashanith <prashanith007@gmail.com>
2022-01-20 12:06:09 +0530

    Faculty Event Dahsboard Endpoint Created

--- server/src/api/routes/faculty/router.js
+++ server/src/api/routes/faculty/router.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const controller = require("../../../services/forum/controller");
+const eventController = require("../../../services/events/controller");
+
 
 router.get("/getForums", controller.getForumsList);
-
+router.get("/dashboardInfo",eventController.getRequests);
 
 module.exports = router;
