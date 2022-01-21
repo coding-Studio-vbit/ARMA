6a30047e816e42c368f3d66ccafc82c8e10781a6
SidMalladi <siddumalladi1234@gmail.com>
2022-01-21 19:28:12 +0530

    update on Students_View

--- client/src/features/faculty/students_view/Students_View.tsx
+++ client/src/features/faculty/students_view/Students_View.tsx
@@ -5,14 +5,84 @@ import Table from "../../../components/CustomTable";
 function Students_View() {
   return (
     <div>
-      <div className="text-arma-dark-blue pt-16 px-14">Personal Details</div>
-      <div className="grid-cols-3 divide-2">
-        <div className="grid grid-cols-3 divide-2">1st</div>
-        <div>2nd</div>
+      <div className="text-arma-dark-blue pt-16 px-14 text-lg">
+        Personal Details
+      </div>
+      <div className="w-4/5 px-14 rounded-[8px]">
+        <div className="w-full border-2 shadow-md  rounded-[16px] overflow-clip">
+          <div className="grid grid-cols-4 gap-4 m-8">
+            <div>
+              <div className="flex space-x-4">
+                <p className="text-arma-grey mb-5 text-xl">First Name:</p>
+              </div>
+            </div>
+            <div>
+              <div className="flex space-x-4">
+                <p className="text-arma mb-5 text-xl">Siddharth</p>
+              </div>
+            </div>
+            <div>
+              <div className="flex space-x-4">
+                <p className="text-arma-grey mb-5 text-xl">Last Name:</p>
+              </div>
+            </div>
+            <div>
+              <div className="flex space-x-4">
+                <p className="text-arma mb-5 text-xl">Malladi</p>
+              </div>
+            </div>
+
+            <div>
+              <div className="flex space-x-4">
+                <p className="text-arma-grey mb-5 text-xl">Year:</p>
+              </div>
+            </div>
+            <div>
+              <div className="flex space-x-4">
+                <p className="text-arma mb-5 text-xl">4</p>
+              </div>
+            </div>
+            <div>
+              <div className="flex space-x-4">
+                <p className="text-arma-grey mb-5 text-xl">Department:</p>
+              </div>
+            </div>
+            <div>
+              <div className="flex space-x-4">
+                <p className="text-arma mb-5 text-xl">CSE</p>
+              </div>
+            </div>
+
+            <div>
+              <div className="flex space-x-4">
+                <p className="text-arma-grey mb-5 text-xl">Section:</p>
+              </div>
+            </div>
+            <div>
+              <div className="flex space-x-4">
+                <p className="text-arma mb-5 text-xl">C</p>
+              </div>
+            </div>
+            <div>
+              <div className="flex space-x-4">
+                <p className="text-arma-grey mb-5 text-xl">Contact:</p>
+              </div>
+            </div>
+            <div>
+              <div className="flex space-x-4">
+                <p className="text-arma mb-5 text-xl">9902917558</p>
+              </div>
+            </div>
+            
+
+          
+
+          </div>
+        </div>
       </div>
 
-      <div className="text-arma-dark-blue pt-16 px-14">Memberships</div>
-      <div className="w-fit">
+      <div className="text-arma-dark-blue pt-16 px-14 text-lg ">Memberships</div>
+      <div className=" px-14">
         <Table
           api="http://localhost:5000/students"
           rowsPerPage={3}
@@ -38,64 +108,70 @@ function Students_View() {
         />
       </div>
 
-      <div className="text-arma-dark-blue pt-16 px-14 ">Events Organised</div>
-      <Table
-        api="http://localhost:5000/students"
-        rowsPerPage={3}
-        buttonsCount={1}
-        headers={[
-          { displayName: "SNo", dataPath: "sno", sortable: true },
-          {
-            displayName: "Forum",
-            dataPath: "forum",
-            sortable: false,
-          },
-          {
-            displayName: "Role",
-            dataPath: "role",
-            sortable: false,
-          },
-          {
-            displayName: "Event Name",
-            dataPath: "eventname",
-            sortable: false,
-          },
-          {
-            displayName: "Duration",
-            dataPath: "forum",
-            sortable: true,
-          },
-        ]}
-      />
+      <div className="text-arma-dark-blue pt-16 px-14 text-lg ">
+        Events Organised
+      </div>
+      <div className=" px-14 ">
+        <Table
+          api="http://localhost:5000/students"
+          rowsPerPage={3}
+          buttonsCount={1}
+          headers={[
+            { displayName: "SNo", dataPath: "sno", sortable: true },
+            {
+              displayName: "Forum",
+              dataPath: "forum",
+              sortable: false,
+            },
+            {
+              displayName: "Role",
+              dataPath: "role",
+              sortable: false,
+            },
+            {
+              displayName: "Event Name",
+              dataPath: "eventname",
+              sortable: false,
+            },
+            {
+              displayName: "Duration",
+              dataPath: "forum",
+              sortable: true,
+            },
+          ]}
+        />
+      </div>
       <div className="text-arma-dark-blue pt-16 px-14">Events Participated</div>
-      <Table
-        api="http://localhost:5000/students"
-        rowsPerPage={3}
-        buttonsCount={1}
-        headers={[
-          { displayName: "SNo", dataPath: "sno", sortable: true },
-          {
-            displayName: "Forum",
-            dataPath: "forum",
-            sortable: false,
-          },
-          {
-            displayName: "Event Name",
-            dataPath: "forum",
-            sortable: false,
-          },
-          {
-            displayName: "Duration",
-            dataPath: "forum",
-            sortable: true,
-          },
-          {
-            displayName: "Other Remarks",
-            dataPath: "other remarks",
-            sortable: false,
-          },
-        ]}
-      />
+      <div className="px-14 ">
+        <Table
+          api="http://localhost:5000/students"
+          rowsPerPage={3}
+          buttonsCount={1}
+          headers={[
+            { displayName: "SNo", dataPath: "sno", sortable: true },
+            {
+              displayName: "Forum",
+              dataPath: "forum",
+              sortable: false,
+            },
+            {
+              displayName: "Event Name",
+              dataPath: "forum",
+              sortable: false,
+            },
+            {
+              displayName: "Duration",
+              dataPath: "forum",
+              sortable: true,
+            },
+            {
+              displayName: "Other Remarks",
+              dataPath: "other remarks",
+              sortable: false,
+            },
+          ]}
+        />
+      </div>
       <div className="grid justify-items-end my-32">
         <button className="outlineBtn">GENERATE</button>
       </div>
