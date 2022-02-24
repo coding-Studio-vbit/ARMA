c3c9b60c3f9a9a8062c879ab6228d38d5027ff3d
Prashanith <prashanith007@gmail.com>
2022-02-24 17:17:03 +0530

    New

--- client/src/features/forum/main_dashboard/Dashboard.tsx
+++ client/src/features/forum/main_dashboard/Dashboard.tsx
@@ -1,6 +1,5 @@
 import { Add } from "@material-ui/icons";
 import EventCard from "./EventCard";
-import StatisticsCard from "./StatisticsCard";
 
 const Dashboard = () => {
   const eventList = [
@@ -39,12 +38,18 @@ const Dashboard = () => {
   return (
     <div>
       {/* Forum Cover */}
-      <div id="forumCoverSection" className="hidden sm:block h-[300px] -mt-5 mx-0 px-0">
+      <div id="forumCoverSection" className="relative hover:opacity-90 hidden sm:block h-[300px] -mt-5 mx-0 px-0">
         <img
-          src="sky.jpg"
+          src="/sky.jpg"
           alt="forum-cover"
           className="w-full h-full object-cover rounded-sm opacity-60"
         />
+        <div className="absolute z-40 bottom-10 right-10">
+            <button className="btn text-sm px-6 py-auto" onClick={()=>{}}>
+              <Add fontSize="small"/>Add Forum Profile
+            </button>
+          
+        </div>
       </div>
 
       <div id="bottomSection" className="mx-0 px-0 sm:flex sm:flex-row-reverse ">
@@ -73,7 +78,7 @@ const Dashboard = () => {
 
         <div
           id="eventCardsSection"
-          className="sm:relative -top-16 mb-4 w-full sm:w-3/5 md:w-2/3 lg:w-5/6 flex flex-wrap justify-center lg:justify-start lg:ml-4">
+          className="hidden sm:relative -top-16 mb-4 w-full sm:w-3/5 md:w-2/3 lg:w-5/6 flex flex-wrap justify-center lg:justify-start lg:ml-4">
           {
             eventList.map((item) => {
               return (
