d26ad7a01578b6ab2655dc84e30f7c272954b866
RevantArk <revantark@gmail.com>
2022-03-03 16:15:28 +0530

    Update event interface to match mongo model

--- client/src/interfaces/event.tsx
+++ client/src/interfaces/event.tsx
@@ -1,28 +1,56 @@
 import { Attendance } from "./attendance";
-import { Forum } from "./user";
+import { Faculty, Forum } from "./user";
 
-type Status = "APPROVAL PENDING"|
-                    "REQUESTED CHANGES"|
-                    "APPROVED"|
-                    "REJECTED"
-                    
+type Status =
+  | "AWAITING BUDGET APPROVAL"
+  | "REQUESTED BUDGET CHANGES"
+  | "BUDGET CHANGES UPDATED"
+  | "BUDGET REJECTED"
+  | "AWAITING SAC APPROVAL"
+  | "REQUESTED CHANGES BY SAC"
+  | "SAC CHANGES UPDATED"
+  | "APPROVED"
+  | "REJECTED"
+  | "COMPLETED";
 
+export interface Equipment{
+  name:string;
+  totalCount :number;
+  facultyIncharge: Faculty
+}
+
+export interface Hall{
+  name:string;
+  block:string;
+  hallInfo:string;
+  capacity:number;
+}
+
+type TimeSlot = "Morning" | "Afternoon"
+interface EventHalls {
+  date:Date;
+  timeSlot:TimeSlot,
+  hall:Hall
+  
+}
 export interface Event {
-    name: string
-      description:string
-      forumID: string | Forum
-      attendanceDocID: string | Attendance
-      eventStatus: Status | 'COMPLETE'
-      hasBudget: boolean
-      budgetStatus: Status
-      eventDates: (Date)[],
-      eventProposalDocPath: {
-        type: String,
-        required: true,
-      },
-      budgetDocPath:string
-      reportDocPath: string
-      mediaFilePaths:(string)[],
-      FOComments:String,
-      SACComments:String,
+  name: string;
+  description: string;
+  forumID: string | Forum;
+  attendanceDocID?: string | Attendance;
+  eventStatus: Status | "COMPLETE";
+  hasBudget?: boolean;
+  budgetStatus?: Status;
+  eventDates?: Date[];
+  eventProposalDocPath?: {
+    type: String;
+    required: true;
+  };
+  equipment : Equipment[];
+  halls : EventHalls[];
+  budgetDocPath?: string;
+  reportDocPath?: string;
+  mediaFilePaths?: string[];
+  FOComments?: String;
+  SACComments?: String;
 }
