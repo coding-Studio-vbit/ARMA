08a5b3aaf3c52517487ccf0ca808cf8588c2ea53
Sai Kiran Boddu <bsaikiran618@gmail.com>
2022-03-05 12:38:18 +0530

    update roll number validation

--- server/src/services/util/rollNumberValidator.js
+++ server/src/services/util/rollNumberValidator.js
@@ -8,20 +8,20 @@ const checkStudentRollNumber = (roll) => {
    * 2 - Branch Code -
    * 2 - Serial Number
    */
+  roll = roll.toUpperCase();
 
-  let year = roll.substring(0, 2);
+  let year = roll.substring(0, 2).trim();
   let allowedYears = [];
-  let currentYear = newDate().getFullYear();
+  let currentYear = new Date().getFullYear();
   for (let i = 0; i < 8; i++)
     allowedYears.push(String(currentYear - i).slice(-2));
-
   //Invalid Year
-  if (!(year in allowedYears)) return false;
+  if (!(allowedYears.includes(year))){console.log("Invalid year"); return false;}
 
   let courseType = roll.substring(4, 5);
 
   //Invalid Course Type
-  if (!(courseType in ["1", "5"])) return false;
+  if (!(["1", "5"].includes(courseType))) {console.log("Invalid course type"); return false;}
 
   let courseCode = roll.substring(5, 6);
   let branchCode = roll.substring(6, 8);
@@ -34,16 +34,19 @@ const checkStudentRollNumber = (roll) => {
 
   for (course in course_structure) {
     if (course_structure[course].code == courseCode) {
-      branchOK = true;
+      courseOK = true;
       let branches = course_structure[course].branches;
       for (branch in branches)
-        if (branch.code == branchCode) {
-          courseOK = true;
+      {
+        if (course_structure[course].branches[branch].code == branchCode) {
+          branchOK = true;
           break;
         }
+      }
       break;
     }
   }
+
   return branchOK && courseOK;
 };
 
