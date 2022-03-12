b7ce549cba9b835274be01aee7f59ae1cfdce7d9
Prashanith <prashanith007@gmail.com>
2022-03-13 04:49:52 +0530

    Profile Added - Backend WIP

--- client/src/features/forum/profile/profile.tsx
+++ client/src/features/forum/profile/profile.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import { AccountCircle } from "@material-ui/icons";
+import './profile.css'
 
-function Profile({url,}) {
+function Profile({url,setUrl}) {
+  const [profileObj, setprofileObj] = useState(null);
+  async function getProfileURL(){
+    setUrl('')
+  }
+  useEffect(() => {
+    // getProfileURL()
+  }, [])
+  
   return (
     <div>
-        {/* {
+        <label className="custom-file-upload">
+          <input type="file" name="file" onChange={
+            (e)=>setprofileObj(URL.createObjectURL(e.target.files[0]))
+          }/>
+          {
+            profileObj==null?
             url.length>0?
-            <img src={url}/>:
-            
-        } */}
+            <img src={url} alt="fuckc"></img>:
+            <AccountCircle className="!text-7xl text-arma-title"/> 
+            :     
+            <img src={profileObj} alt="fuckc"></img>       
+          }
+        </label>          
     </div>
   )
 }
