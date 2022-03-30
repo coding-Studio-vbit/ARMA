ccb941431a5c89a2dbe62e93b341e8c6ed43e0cb
Sai Kiran Boddu <bsaikiran618@gmail.com>
2022-03-30 18:46:15 +0530

    event card update

--- admin/admin-client/src/Components/Auth/login.tsx
+++ admin/admin-client/src/Components/Auth/login.tsx
@@ -4,11 +4,11 @@ import { InputField } from "../InputField/InputField";
 import { login } from "./authService";
 import { VisibilityOff, Visibility } from "@material-ui/icons";
 import { useUser } from "../../Provider/userProvider";
-import Spinner  from "../Spinner/Spinner";
+import Spinner from "../Spinner/Spinner";
 
 function Login() {
   let navigate = useNavigate();
-  const { user,setUser } = useUser();
+  const { user, setUser } = useUser();
   const [emailError, setEmailError] = useState<string>();
   const [passwordError, setPasswordError] = useState<string>();
   const [email, setEmail] = useState<String>("");
@@ -19,7 +19,7 @@ function Login() {
   useEffect(() => {
     if (user) {
       navigate("/Admins", { replace: true });
-    } 
+    }
   }, [user]);
 
   const validateEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -54,60 +54,57 @@ function Login() {
       <p className="text-[#263238] mb-8 text-xl text-center">
         Automating and Digitalizing Event Organization
       </p>
-     
-       
-      <p className="text-4xl mb-8 text-[#6FB4DC]" >Admin Portal</p>
-      <form className="flex flex-col items-center" >
-      <InputField
-        className="mb-5"
-        name="Email"
-        error={emailError}
-        onChange={(e) => {
-          validateEmail(e);
-        }}
-      />
-      <div className="relative mb-8">
+
+      <p className="text-4xl mb-8 text-[#6FB4DC]">Admin Portal</p>
+      <form className="flex flex-col items-center">
         <InputField
-          className="mb-3"
-          name="Password"
-          type={`${!showPassword && "password"}`}
-          error={passwordError}
+          className="mb-5"
+          name="Email"
+          error={emailError}
           onChange={(e) => {
-            validatePass(e);
+            validateEmail(e);
           }}
         />
-        {showPassword ? (
-          <Visibility
-            className="absolute top-[0.85rem] right-3 text-arma-title cursor-pointer"
-            onClick={() => setShowPassword(false)}
+        <div className="relative mb-8">
+          <InputField
+            className="mb-3"
+            name="Password"
+            type={`${!showPassword && "password"}`}
+            error={passwordError}
+            onChange={(e) => {
+              validatePass(e);
+            }}
           />
-        ) : (
-          <VisibilityOff
-            className="absolute top-[0.85rem] right-3 text-arma-title cursor-pointer"
-            onClick={() => setShowPassword(true)}
-          />
-        )}
-      </div>
-      {error && <span className="text-arma-red mb-3">{error}</span>}
-      <LoginButton
-        onClick={async () => {
+          {showPassword ? (
+            <Visibility
+              className="absolute top-[0.85rem] right-3 text-arma-title cursor-pointer"
+              onClick={() => setShowPassword(false)}
+            />
+          ) : (
+            <VisibilityOff
+              className="absolute top-[0.85rem] right-3 text-arma-title cursor-pointer"
+              onClick={() => setShowPassword(true)}
+            />
+          )}
+        </div>
+        {error && <span className="text-arma-red mb-3">{error}</span>}
+        <LoginButton
+          onClick={async () => {
+            if (!email || !password || emailError || passwordError) {
+              setError("Fill the details !");
+              return;
+            }
 
-          if(!email || !password || emailError || passwordError ){
-            setError('Fill the details !')
-            return
-          }
-  
+            const res = await login(email, password);
 
-          const res = await login(email, password);
-
-          if (res.status === 1) {
-            setUser(res.response.user)
-          } else {
-            setError(res.response);
-          }
-        }}
-      />
-        </form>
+            if (res.status === 1) {
+              setUser(res.response.user);
+            } else {
+              setError(res.response);
+            }
+          }}
+        />
+      </form>
       <p className="text-arma-title font-medium">Forgot Password?</p>
     </div>
   );
