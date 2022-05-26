eb392df7711c557a43cf6b206231f84bd1d23cc1
Sai Kiran Boddu <bsaikiran618@gmail.com>
2022-05-26 11:06:13 +0530

    changed to https server

--- server/src/app.js
+++ server/src/app.js
@@ -2,16 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const loaders = require("./loaders/loaders");
+const fs = require("fs");
+const path = require("path")
+const https = require("https");
 
-const port = 5000 || process.env.PORT
+const port = 5000 || process.env.PORT;
 
 const startServer = () => {
   const app = express();
 
   loaders({ app, mongoose });
 
-  app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
+  const sslServer = https.createServer(
+    {
+      key: fs.readFileSync(path.join(__dirname, '../cert',"key.pem")),
+      cert: fs.readFileSync(path.join(__dirname, '../cert', "cert.pem"))
+    },
+    app
+  );
+
+  sslServer.listen(port, () => {
+    console.log(`Listening at https://localhost:${port}`);
   });
 };
 
